4ec9a49d87ea4eb50350d970b3d0e1ea48828fdf
gon <y2gon2@gmail.com>
2023-12-03 11:50:44 +0900

    기본적인 LiveView Authentication 적용
    LiveView 라이브러리가 제공하는 Authentication 을 적용
    
    * Authentication Generator 사용을 위한 phx.gen 추가
    ```
    > mix phx.gen
    ```
    이 중 auth 관련 generator 사용
    ```
    > mix phx.gen.auth Accounts User uers
    ```
    (by ChatGPT)
    1. Context ('Accounts'):
    Context는 Phoenix에서 도메인 특정 로직을 캡슐화하는 모듈입니다. 이는 데이터베이스와의 상호작용을 관리하고, 애플리케이션의 나머지 부분에 서비스 API를 제공합니다.
    "Accounts"는 이 예제에서 생성될 context의 이름입니다. 이 context는 인증과 관련된 모든 함수와 로직을 포함하게 됩니다.
    
    2. Schema ('User'):
    Schema는 데이터베이스 테이블의 구조를 설명하는 Elixir 모듈입니다. 이는 테이블의 각 열에 해당하는 필드와 그 타입을 정의합니다.
    "User"는 사용자 데이터를 나타내는 schema의 이름입니다. 이 schema는 사용자 데이터를 저장하고 쿼리하는 데 사용됩니다.
    
    3. Plural name of the schema ('users'):
    이것은 데이터베이스 내에서 사용될 테이블의 이름입니다. Phoenix는 일반적으로 schema 이름의 복수형을 테이블 이름으로 사용합니다.
    "users"는 "User" schema에 대응하는 데이터베이스 테이블의 이름이 됩니다.
    
    ```
    > mix deps.get
    ```
    
    * Run Migration
    ```
    > mix ecto.migrate
    ```
    해당 설정 이후 기존 `live "/guess", WrongLive` 를 :require_authenticated_user plug 를 실행하는 scope 로 옮겨서 추가해준다.
    
    따라서 해당 경로 접속은 Authentication 이 필요하며, 해당 인증은 LiveView 에서 재공하는 login / signup page 를 사용하여 회원 등록을 손쉽게 적용할 수 있게 된다.

---
+++ lib/pento_web/router.ex
@@ -0,0 +1,105 @@
+defmodule PentoWeb.Router do
+  use PentoWeb, :router
+
+  import PentoWeb.UserAuth
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, html: {PentoWeb.Layouts, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug :fetch_current_user
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", PentoWeb do
+    pipe_through :browser
+
+    get "/", PageController, :home
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", PentoWeb do
+  #   pipe_through :api
+  # end
+
+  # Enable LiveDashboard and Swoosh mailbox preview in development
+  if Application.compile_env(:pento, :dev_routes) do
+    # If you want to use the LiveDashboard in production, you should put
+    # it behind authentication and allow only admins to access it.
+    # If your application does not have an admins-only section yet,
+    # you can use Plug.BasicAuth to set up some basic authentication
+    # as long as you are also using SSL (which you should anyway).
+    import Phoenix.LiveDashboard.Router
+
+    scope "/dev" do
+      pipe_through :browser
+
+      live_dashboard "/dashboard", metrics: PentoWeb.Telemetry
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  #라우터에서 pipe_through 사용하기:
+  # Phoenix에서는 pipe_through를 사용하여 라우트를 특정 플러그 파이프라인에 연결할 수 있다.
+  # 이는 라우트가 처리되기 전에 특정 플러그들이 실행함.
+  # 즉 일반적인 HTTP 요청에 대해서는, 이런 방식으로 인증과 같은 필수적인 절차를 거침.
+  # 그러나 LiveView에서는 WebSocket을 통한 통신으로 인해 이러한 절차를 건너뛰게 됨.
+  # 따라서, 라우터에서 일반 라우트뿐만 아니라, HTTP GET 요청으로 시작하는 live 라우트에 대해서도
+  # pipe_through를 사용해야 함. 따라서, LiveView가 처음 로드될 때 필요한 보안 절차를 거치게 됨.
+  #
+  # Live 뷰 마운트 시 인증 및 권한 부여 로직 구현:
+  # LiveView가 WebSocket을 통해 연결된 후, 페이지 간의 이동(라이브 리디렉션)은
+  # WebSocket 세션 내에서 이루어지므로 추가적인 HTTP 요청이나 플러그 파이프라인을 거치지 않음.
+  # 즉, 한 LiveView에서 다른 LiveView로 이동할 때, 기존에 설정된 보안 절차를 다시 거치지 않는다는 것을 의미.
+  # 이것이 보안상의 허점이 될 수 있으므로, 이를 방지하기 위해, 각 LiveView가 마운트될 때
+  # (즉, LiveView가 시작될 때) 인증 및 권한 부여 로직을 실행해야 함.
+  # 따라서, LiveView 간의 이동이 발생해도 각 뷰에서 필요한 보안 검사를 수행할 수 있음.
+  scope "/", PentoWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{PentoWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", PentoWeb do
+    # pipe_through를 사용하여 라우트를 특정 플러그 파이프라인에 연결
+    # 이는 router 가 처리되기 전에 특정 플러그(:require_authenticated_user)들이 실행되도록 함.
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      # root_layout: {PentoWeb.Layouts, :root}, # 해당 인자는 이미 default 라서 적용해도 동일함.
+      on_mount: [{PentoWeb.UserAuth, :ensure_authenticated}] do
+      # 어떤 browser HTTP request 에 대해서 authentication 이 필요하진 설정
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+      live "/guess", WrongLive
+    end
+  end
+
+  scope "/", PentoWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{PentoWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
+    end
+  end
+end
